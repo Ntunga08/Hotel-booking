41153c016b42215bea55ee13683fcff448c0d999
ntunga08 <samwelbuluba22@gmail.com>
2025-09-14 13:10:26 +0300

    payment  crud operation addition

--- app/crud/payment.py
+++ app/crud/payment.py
@@ -10,10 +10,17 @@ from sqlalchemy import func
 
 """payment CRUD operations """
 async def create_payment(db: AsyncSession, payment: schemas.PaymentCreate) -> models.Payment:
-    db_payment = models.Payment(**payment.dict)
+    db_payment = models.Payment(**payment.dict())
     db.add(db_payment)
     await db.commit()
-    await db.refresh (db_payment)  
-    return payment  
+    await db.refresh(db_payment)
+    return db_payment
 
-async def                                            
+async def get_payment(db: AsyncSession, payment_id: int) -> Optional[models.Payment]:
+    """get payment by id"""
+    result = await db.execute(select(models.Payment).where(models.Payment.id == payment_id))
+    return result.scalars().first()
+
+async def get_payments(db: AsyncSession, skip: int = 0, limit: int = 100) -> List[models.Payment]:
+    result = await db.execute(select(models.Payment).offset(skip).limit(limit))
+    return result.scalars().all()
